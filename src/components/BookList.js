cf9956ed0b507bd93b884d9881b4b5eebf6c1094
joe panetta <jlpanetta1681@icloud.com>
2023-01-26 12:30:24 -0500

    Feature: front-end initial scaffold JLP

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div>
+      <h1>Book List</h1>
+      <p>
+        Here you can find a list of books that have been reviewed by other
+        users.
+      </p>
+    </div>
+  );
+};
+
+export default BookList;
